1f7b02d59e3151d0c3ca92457329f87b9cdf2828
Mateus Nascimento <mateusnl.vg@gmail.com>
2023-02-14 22:18:38 -0300

    user id e tratamento de exceção

--- src/main/java/com/mattnekoo/workshopmongo/services/UserService.java
+++ src/main/java/com/mattnekoo/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mattnekoo.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mattnekoo.workshopmongo.domain.User;
 import com.mattnekoo.workshopmongo.repositories.UserRepository;
+import com.mattnekoo.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
