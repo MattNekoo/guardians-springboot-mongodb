153e894f9e868aeaf531519116a1c827932c4e6c
Mateus Nascimento <mateusnl.vg@gmail.com>
2023-02-21 10:10:25 -0300

    update filme

--- src/main/java/com/mattnekoo/workshopmongo/resources/FilmeResources.java
+++ src/main/java/com/mattnekoo/workshopmongo/resources/FilmeResources.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +43,17 @@ public class FilmeResources {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Filme> update(@PathVariable String id, @RequestBody Filme obj) {
+		obj = service.update(obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/mattnekoo/workshopmongo/services/FilmeService.java
+++ src/main/java/com/mattnekoo/workshopmongo/services/FilmeService.java
@@ -1,14 +1,19 @@
 package com.mattnekoo.workshopmongo.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.mattnekoo.workshopmongo.entities.Filme;
 import com.mattnekoo.workshopmongo.repositories.FilmeRepository;
+import com.mattnekoo.workshopmongo.services.exception.DatabaseException;
 import com.mattnekoo.workshopmongo.services.exception.ObjectNotFoundException;
+import com.mattnekoo.workshopmongo.services.exception.ResourceNotFoundException;
 
 @Service
 public class FilmeService {
@@ -22,11 +27,41 @@ public class FilmeService {
 
 	public Filme findById(String id) {
 		Optional<Filme> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
+	/* INSERT FILME */
 	public Filme insert(Filme obj) {
 		return repository.save(obj);
 	}
 
+	/* DELETE FILME */
+	public void delete(String id) {
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	/* UPDATE FILME */
+	public Filme update(Filme obj) {
+		try {
+			Optional<Filme> newObj = repository.findById(obj.getId());
+			Filme filme = newObj.get();
+			updateData(filme, obj);
+			return repository.save(filme);			
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+	}
+
+	// AUXILIAR METHOD FOR FILME UPDATE
+	private void updateData(Filme entity, Filme obj) {
+		entity.setnomeFilme(obj.getnomeFilme());
+		entity.setanoFilme(obj.getanoFilme());
+		entity.setdscFilme(obj.getdscFilme());
+	}
 }
